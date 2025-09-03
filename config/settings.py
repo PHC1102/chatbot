8f3b5ec834b94d4a356f54feb489c0c9ad8fd5df
PHC1102 <phanhuycuong1205@gmail.com>
2025-09-04 00:01:42 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ class Config:
     OPENROUTER_URL = "https://openrouter.ai/api/v1"
     
     # Model Configuration
-    LLM_MODEL = "openai/gpt-oss-20b:free"
+    LLM_MODEL = "qwen/qwen3-4b:free"
     VISION_MODEL = "Jayanth2002/dinov2-base-finetuned-SkinDisease"
     
     # Chat Configuration
